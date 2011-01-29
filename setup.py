a90f0d9520f8c504e2eb89ac8b620eb46a063918
mviera <manuel.viera.tirado@gmail.com>
2011-01-29 17:16:29 +0100

    setup.py describing the project.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name='trac_launcher',
+    version='0.1',
+    url='http://github.com/mviera/easyTrac',
+    license='GPL',
+    description='Trac tool for running it through zc.buildout',
+    author='Manuel Viera <manuel.viera.tirado@gmail.es>',
+    classifiers=[
+        'Development Status :: early stages',
+        'Intended Audience :: Developers',
+        'License :: EUPL',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    py_modules=['trac_launcher'],
+    install_requires=[
+        'distribute==0.6.14',
+    ],
+)
---
+++ trac_launcher.py
@@ -0,0 +1,2 @@
+def not_used():
+    print "This is never used."
